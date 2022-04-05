f823ed69fb26dab043255ee3ca3e281e9eb941cb
dinoslow <73374146+dinoslow@users.noreply.github.com>
2022-04-05 20:57:32 +0800

    add gif

--- dump/dump.sql
+++ dump/dump.sql
@@ -1,5 +1,5 @@
 CREATE TABLE urls (
     url_id serial PRIMARY KEY, 
-    link VARCHAR(255) NOT NULL,
+    url VARCHAR(255) NOT NULL,
     expireat VARCHAR(255) NOT NULL
 );
--- main.go
+++ main.go
@@ -27,8 +27,8 @@ const (
 	user     = "postgres"
 	password = "postgres"
 	dbname   = "postgres"
-	// domain = "http://localhost:3001/"
-	domain = "http://ec2-52-197-102-90.ap-northeast-1.compute.amazonaws.com/"
+	domain = "http://localhost:3001/"
+	// domain = "http://ec2-52-197-102-90.ap-northeast-1.compute.amazonaws.com/"
 )
 
 func setupDB() *sql.DB {
@@ -54,6 +54,7 @@ func main() {
 }
 
 func Get(w http.ResponseWriter, r *http.Request) {
+	// 1. record time when GET Method is called
 	loc, err := time.LoadLocation("")
 	if err != nil {
 		w.WriteHeader(http.StatusNotFound)
@@ -62,24 +63,28 @@ func Get(w http.ResponseWriter, r *http.Request) {
 	}
 	now := time.Now().In(loc).Format(time.RFC3339)
 
+	// 2. get param from url
 	vars := mux.Vars(r)
 	url_id := vars["url_id"]
 
+	// 3. using param to search origin url in db
 	var originalUrl string
 	db := setupDB()
-	err = db.QueryRow("SELECT link FROM urls WHERE url_id = $1 AND expireat > $2", url_id, now).Scan(&originalUrl)
+	err = db.QueryRow("SELECT url FROM urls WHERE url_id = $1 AND expireat > $2", url_id, now).Scan(&originalUrl)
 	if err != nil {
 		w.WriteHeader(http.StatusNotFound)
 		w.Write([]byte("NON-EXISTENT URL or URL EXPIRED"))
 		return
 	}
 
+    // 4. redirect to the origin website while exist or not expired
 	http.Redirect(w, r, originalUrl, http.StatusSeeOther)
 
 	defer db.Close()
 }
 
 func Post(w http.ResponseWriter, r *http.Request) {
+	// 1. get params from request body
 	var req URL
 	err := json.NewDecoder(r.Body).Decode(&req)
 	if err != nil {
@@ -93,9 +98,10 @@ func Post(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// 2. insert url, expireDate to db and return url index
 	var returnID int
 	db := setupDB()
-	err = db.QueryRow("INSERT INTO urls(link, expireAt) VALUES($1, $2) returning url_id", req.Url, req.ExpireAt).Scan(&returnID)
+	err = db.QueryRow("INSERT INTO urls(url, expireAt) VALUES($1, $2) returning url_id", req.Url, req.ExpireAt).Scan(&returnID)
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		fmt.Println("Error in insert")
@@ -105,11 +111,14 @@ func Post(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
 
+
+    // 3. generate JSON file
 	id := strconv.Itoa(returnID)
 	returnUrl := make(map[string]string)
 	returnUrl["id"] = id
 	returnUrl["shortUrl"] = domain + id
 
+	// 4. write server response
 	response, err := json.Marshal(returnUrl)
 	if err != nil {
 		log.Fatalf("Error happened in JSON marshal. Err: %s", err)
